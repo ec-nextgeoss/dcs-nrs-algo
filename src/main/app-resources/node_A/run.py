e59ec3546cf27f0d6e02184cd126c20485a5534a
WillemNieuwenhuis <w.nieuwenhuis@utwente.nl>
2018-05-16 15:36:07 +0200

    Added search node and function

--- src/main/app-resources/node_A/run.py
+++ src/main/app-resources/node_A/run.py
@@ -2,6 +2,9 @@
 
 import sys
 import os
+import cioppy
+
+ciop = cioppy.Cioppy()
 
 sys.path.append(os.environ['_CIOP_APPLICATION_PATH'] + '/util')
 from util import log_input
@@ -10,7 +13,19 @@ from util import pass_next_node
 # Input references come from STDIN (standard input) and they are retrieved
 # line-by-line.
 for input in sys.stdin:
+    start = ciop.getparam('startdate')
+    stop = ciop.getparam('enddate')
+    boundingarea = ciop.getparam('bounding_area')
+    prod_type = ciop.getparam('type')
+    log_input('searching with:')
+    log_input(start)
+    log_input(stop)
+    log_input(boundingarea)
+    log_input(prod_type)
+    search_params = dict([('start',start),('stop', stop), ('geom', boundingarea), ('pt', prod_type)]) 
+    search_result = ciop.search(end_point="https://catalog.terradue.com/sentinel2/search", params=search_params)
     # Log the input
     log_input(input)
     # Just pass the input reference to the next node
-    pass_next_node(input)
+    #pass_next_node(input)
+    ciop.publish(sources=search_result, mode='silent')
